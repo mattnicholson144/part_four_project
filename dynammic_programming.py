ead989428a586e6cb87483dc96ba2f24eb5ee7bc
mattnicholson144 <31492320+mattnicholson144@users.noreply.github.com>
2018-03-15 15:04:16 +1300

    Created dynamic programming script

---
+++ dynammic_programming.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 14:38:41 2018
+
+Dynamic Programming for Strategy 
+
+@author: matthnicholson
+"""
+#Import Appropriate libraries
+
+#Create Manhattan Function
+
+#Create value function
