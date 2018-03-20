9ebd3a95743a37f97d77efa9a3e88286c735e0ee
mattnicholson144 <31492320+mattnicholson144@users.noreply.github.com>
2018-03-21 12:11:48 +1300

    dp_policy generation started
    not sure on approach for populating v table as I'm not sure about the recursion.
    
    I am not sure I am capturing the two player depth/accounting for an optimal response

---
+++ dp_policy.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 21 11:10:34 2018
+
+@author: matth
+"""
+
+#Import Appropriate libraries
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+def initialise_policy_table(turns, n, m):
+    #initialise table
+    possible_states = n*n*m*m
+    policy_table = np.array(shape = (turns, n, m, n, m))
+    
+    t = 0
+    for i in range(n):
+        for j in range(m):
+            for k in range(n):
+                for l in range(m):
+                     policy_table[t][i][j][k][l]=manhattan_dist(i,j) - manhattan_dist(k,l)
+
+    return policy_table
+
+
+#Create Manhattan Function
+def manhattan_dist(i,j):
+    #add complexity with triangular regio later
+    return i
+
+#create possible moves function
+def possible_moves_player(i,j,k,l, I, J, K, L, iORk, n, m):
+    #i, j, k, l are the game state for the current turn
+    #I, J, are the game state from the previous turn for the moving player
+    #n, m, are the dimensions of the board.
+    #No 180s 
+    #No moving on top of each other
+    
+    #Create list of possible moves
+    states = list([])
+    #if the opponent isn't at the point we want to move and if the player 
+    #hasn't just come from there and they can move 
+    if iORk ==1:
+        
+        #can the player move up
+        if (([i-1, j] != [k, l]) and ([i-1, j] !=[I,J]) and i > 0):
+            state1 = [i-1, j, k, l]
+            states.append(state1)
+        
+        #can the player move down
+        if (([i+1, j] != [k, l]) and ([i+1, j] !=[I,J]) and i < n):
+            state2 = [i+1, j, k, l]
+            states.append(state2)
+        
+        #can the player move left
+        if (([i, j-1] != [k, l]) and ([i, j-1] !=[I,J]) and j > 0):
+            state3 = [i, j-1, k, l]
+            states.append(state3)
+    
+            
+        #can the player move right
+        if (([i, j+1] != [k, l]) and ([i, j+1] !=[I,J]) and j < m):
+            state3 = [i, j+1, k, l]
+            states.append(state3)
+    
+    elif iORk == -1:
+        #can the player move up
+        if (([i, j] != [k-1, l]) and ([k-1, l] !=[K,L]) and k > 0):
+            state1 = [i, j, k-1, l]
+            states.append(state1)
+        
+        #can the player move down
+        if (([i, j] != [k+1, l]) and ([k+1, l] !=[K,L]) and k < n):
+            state2 = [i, j, k+1, l]
+            states.append(state2)
+        
+        #can the player move left
+        if (([i, j] != [k, l-1]) and ([k, l-1] !=[K,L]) and l > 0):
+            state3 = [i, j, k, l-1]
+            states.append(state3)
+    
+            
+        #can the player move right
+        if (([i, j] != [k, l+1]) and ([k, l+1] !=[K,L]) and l < m):
+            state3 = [i, j, k, l+1]
+            states.append(state3)
+    
+    return states
+
+#like a cost function in the dynamic programming example in 760 notes
+#calulates the difference in value between 2 states
+def value_diff_func(new_state, previous_state): 
+    value_previous_state    = manhattan_dist(previous_state[0], previous_state[1]) - (manhattan_dist(previous_state[2], previous_state[3]))
+    value_new_state         = manhattan_dist(new_state[0], new_state[1]) - (manhattan_dist(new_state[2], new_state[3]))
+    return value_new_state - value_previous_state
+
+
+def populate_policy_table(policy_table, turns, n, m):
+    
+    for t in range(1, len(turns)):
+        for i in range(n):
+            for j in range(m):
+                for k in range(n):
+                    for l in range(m):
+                         policy_table[t][i][j][k][l]=max
+
+    return policy_table
+    
+
+#i = 1
+#j = 1
+#k = 1
+#l = 3
+#I = 1
+#J = 0
+#K = 1
+#L = 2
+#n = 4
+#m = 5
+##gamestate = [i,j,k,l]
+#turns = 2
+#iORk = -1
+#
+#score, gamestate = game_function(gamestate, turns, iORk)
+#print('End State')
+#print(gamestate)
+#print('Score')
+#print(score)
+
+#possible_moves_p1 = possible_moves_player(i,j,k,l, I, J,K, L, iORk, n, m)
+#print(possible_moves_p1)
+
+endstate = [0,1,0,3]
+count = 0
+for i in range(4):
+    for j in range(5):
+        for k in range(4):
+            for l in range(5):
+                count = count + 1
+
+
+
--- dynammic_programming.py
+++ dynammic_programming.py
@@ -11,91 +11,106 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 
-#Create grid
-n = 4
-m = 5
-grid = [[[y,x] for x in range(m)] for y in range(n)] 
 
 #Create Manhattan Function
 def manhattan_dist(i,j):
     #add complexity with triangular regio later
     return i
 
-def possible_moves_player(i,j,k,l):
+def possible_moves_player(i,j,k,l, I, J, K, L, iORk, n, m):
     #i, j, k, l are the game state for the current turn
     #I, J, are the game state from the previous turn for the moving player
-    #Force players to move
+    #n, m, are the dimensions of the board.
     #No 180s 
     #No moving on top of each other
-    movestates = list([])
-    if (i!=k and j!= l)
     
+    #Create list of possible moves
+    states = list([])
+    #if the opponent isn't at the point we want to move and if the player 
+    #hasn't just come from there and they can move 
+    if iORk ==1:
+        
+        #can the player move up
+        if (([i-1, j] != [k, l]) and ([i-1, j] !=[I,J]) and i > 0):
+            state1 = [i-1, j, k, l]
+            states.append(state1)
+        
+        #can the player move down
+        if (([i+1, j] != [k, l]) and ([i+1, j] !=[I,J]) and i < n):
+            state2 = [i+1, j, k, l]
+            states.append(state2)
+        
+        #can the player move left
+        if (([i, j-1] != [k, l]) and ([i, j-1] !=[I,J]) and j > 0):
+            state3 = [i, j-1, k, l]
+            states.append(state3)
     
-    return
+            
+        #can the player move right
+        if (([i, j+1] != [k, l]) and ([i, j+1] !=[I,J]) and j < m):
+            state3 = [i, j+1, k, l]
+            states.append(state3)
+    
+    elif iORk == -1:
+        #can the player move up
+        if (([i, j] != [k-1, l]) and ([k-1, l] !=[K,L]) and k > 0):
+            state1 = [i, j, k-1, l]
+            states.append(state1)
+        
+        #can the player move down
+        if (([i, j] != [k+1, l]) and ([k+1, l] !=[K,L]) and k < n):
+            state2 = [i, j, k+1, l]
+            states.append(state2)
+        
+        #can the player move left
+        if (([i, j] != [k, l-1]) and ([k, l-1] !=[K,L]) and l > 0):
+            state3 = [i, j, k, l-1]
+            states.append(state3)
+    
+            
+        #can the player move right
+        if (([i, j] != [k, l+1]) and ([k, l+1] !=[K,L]) and l < m):
+            state3 = [i, j, k, l+1]
+            states.append(state3)
+    
+    return states
 
 
 
 
 #Create value function
-def value_function(i, j, k, l, depth, iORk):
+def value_function(i, j, k, l, I, J, K, L,depth, iORk):
     #i, j represent position of player 1
-    #k, l represent position of player 2 
+    #k, l represent position of player 2
+    #I, J represent the previous state 
     #depth controls depth of tree
     #iORk is one if i or j are moving and -1 of k and l are moving
     #game state var
     valuestate=[i,j,k,l]
+    
     if depth == 0:
         return (manhattan_dist(k,l) - manhattan_dist(i,j)), valuestate
     elif depth > 0 and iORk == 1:
-        movestates = list([])
+        
+        possible_moves = possible_moves_player(i,j,k,l,I,J, iORk,4,5)
         moveValues = np.array([])
         
-        if i < 4:
-            move1, move1state = value_function(i+1, j, k ,l, depth-1, iORk)
-            moveValues = np.append(moveValues, move1)
-            movestates.append(move1state)
-
-        if i > 0:
-            move2, move2state = value_function(i-1, j, k ,l, depth-1, iORk)
-            moveValues = np.append(moveValues, move2)
-            movestates.append(move2state)
-
-        if j < 5:
-            move3, move3state = value_function(i, j+1, k ,l, depth-1, iORk)
-            moveValues = np.append(moveValues, move3)
-            movestates.append(move3state)
-
-        if j > 0:
-            move4, move4state = value_function(i, j-1, k ,l, depth-1, iORk)
-            moveValues = np.append(moveValues, move4)
-            movestates.append(move4state)
-
-
+        for a in possible_moves:
+            moveValues[a] = value_function(possible_moves[a][0], possible_moves[a][1], possible_moves[a][2], possible_moves[a][3], I, J, depth-1, iORk)[0]
+            
         index = np.argmax(moveValues)
-        return moveValues[index], movestates[index]
+        return moveValues[index], possible_moves[index]
     
     elif depth > 0 and iORk == -1:
-        movestates = list([])
+        
+        possible_moves = possible_moves_player(i,j,k,l,I,J, iORk,4,5)
         moveValues = np.array([])
-        if k < 4:
-            move1, move1state = value_function(i, j, k+1 ,l, depth-1, iORk)
-            moveValues = np.append(moveValues, move1)
-            movestates.append(move1state)
-
-        if k > 0:
-            move2, move2state = value_function(i, j, k-1 ,l, depth-1, iORk)
-            moveValues = np.append(moveValues, move2)
-            movestates.append(move2state)
-
-        if l < 5:
-            move3, move3state = value_function(i, j, k ,l+1, depth-1, iORk)
-            moveValues = np.append(moveValues, move3)
-            movestates.append(move3state)
-
-        if l > 0:
-            move4, move4state = value_function(i, j, k ,l-1, depth-1, iORk)
-            moveValues = np.append(moveValues, move4)
-            movestates.append(move4state)
+        
+        for a in possible_moves:
+            moveValues[a] = value_function(possible_moves[a][0], possible_moves[a][1], possible_moves[a][2], possible_moves[a][3], I, J, depth-1, iORk)[0]
+            
+        index = np.argmax(moveValues)
+        return moveValues[index], possible_moves[index]
         
 
         index = np.argmin(moveValues)
@@ -126,14 +141,23 @@ def game_function(gamestate, turns_remaining, iORk):
 
 i = 1
 j = 1
-k = 2
+k = 1
 l = 3
-gamestate = [i,j,k,l]
-turns = 6
-iORk = 1
+I = 1
+J = 0
+K = 1
+L = 2
+n = 4
+m = 5
+#gamestate = [i,j,k,l]
+#turns = 6
+iORk = -1
+#
+#score, gamestate = game_function(gamestate, turns, iORk)
+#print('End State')
+#print(gamestate)
+#print('Score')
+#print(score)
 
-score, gamestate = game_function(gamestate, turns, iORk)
-print('End State')
-print(gamestate)
-print('Score')
-print(score)
+possible_moves_p1 = possible_moves_player(i,j,k,l, I, J,K, L, iORk, n, m)
+print(possible_moves_p1)
